07b4920084d6bde9e07fa3103a9639b48d2ccb18
Azazel 00 <pfbhjdfhn@mail.ru>
2023-10-22 16:26:44 +0500

    Tast1

--- Tyuiu.ZairovAR.Sprint3.Task1.V12.Lib/DataService.cs
+++ Tyuiu.ZairovAR.Sprint3.Task1.V12.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.ZairovAR.Sprint3.Task1.V12.Lib
     {
         public double GetSumSeries(int value, int startValue, int stopValue)
         {
-            double multSeries = 0 ;
+            double multSeries = 0;
             while (startValue <= stopValue)
             {
                 multSeries = multSeries + (Math.Pow((1 / (Math.Pow(startValue, value))), 2));
--- Tyuiu.ZairovAR.Sprint3.Task1.V12.Test/DataServiceTest.cs
+++ Tyuiu.ZairovAR.Sprint3.Task1.V12.Test/DataServiceTest.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.ZairovAR.Sprint3.Task1.V12.Test
             DataService ds = new DataService();
             int startValue = 1;
             int stopValue = 10;
-            int value = 5 ;
+            int value = 5;
             double res = ds.GetSumSeries(value, startValue, stopValue);
             double wait = 1.001;
             Assert.AreEqual(wait, res);
